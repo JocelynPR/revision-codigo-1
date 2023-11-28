f2d77e7e9fde3642ca2411d4129901c41d4a4740
Jocelyn Peña <jocelyn.p.reyes332b@gmail.com>
2023-11-27 20:53:39 -0600

    Se agrega ejercicio revision de codigo 1

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,19 +4,24 @@ const $n = document.querySelector('name');
 const $b = document.querySelector('#blog');
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+async function displayUser(username) { //se agrega async
   $n.textContent = 'cargando...';
+  try{  //se agrega bloque try y catch
   const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  const data = await response.json();  //se agrega const data
+  console.log(data);   //se agrega console.log 
+  $n.textContent = `${data.name}`;  // se cambian comillas por (``)
+  $b.textContent = `${data.blog}`;   // se cambian comillas por (``)
+  $l.textContent = `${data.location}`;  // se cambian comillas por (``)
+  }catch(err){ 
+    handleError(err);
+  }
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}`;  // se agrega (;)
 }
 
-displayUser('stolinski').catch(handleError);
+displayUser('stolinski');
